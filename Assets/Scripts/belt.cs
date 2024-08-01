2dfe26cd7d673045fcc3de2757b971a33b9d7a60
sosonamumu <barwui_min@naver.com>
2024-08-02 03:48:52 +0900

    belt

---
+++ Assets/Scripts/belt.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Callbacks;
+using UnityEngine;
+public class belt : MonoBehaviour
+{
+    [SerializeField] float power;
+    GameObject player;
+    void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.CompareTag("Player")){
+            player.transform.position += new Vector3(power, 0, 0);
+        }
+    }
+    void OnCollisionStay2D(Collision2D other){
+        if(other.gameObject.CompareTag("Player")){
+            player.transform.position += new Vector3(power, 0, 0);
+        }
+    }
+
+    void Update(){
+        if(player == null){
+            player = GameObject.FindWithTag("Player");
+        }
+    }
+}
