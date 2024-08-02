6979b719183e2adf0e18f829396d68ebe59fad71
geonheegit <101814977+geonheegit@users.noreply.github.com>
2024-08-03 05:55:09 +0900

    memo for big door logic

---
+++ Assets/Scripts/big_door.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class big_door : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // power_station.cs, fan_control_tower.cs에서 로직 가져와서 만들기.
+    // yellow keycard & power 필요함.
+}
