0e75e32f83ba2c9dd802888cccf02c7126ee7b01
geonheegit <101814977+geonheegit@users.noreply.github.com>
2024-08-11 13:44:20 +0900

    puzzle DONE

--- Assets/Scripts/Puzzle1/Puzzle1_gear.cs
+++ Assets/Scripts/Puzzle1/Puzzle1_gear.cs
@@ -6,6 +6,7 @@ public class Puzzle1_gear : MonoBehaviour
 {
     [SerializeField] GameObject box_spawn;
     private AudioSource box_break_SFX;
+    [SerializeField] GameObject box_break_effect_obj;
     void Start()
     {
         box_break_SFX = transform.parent.GetComponent<AudioSource>();
@@ -20,10 +21,21 @@ public class Puzzle1_gear : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Interactives"){
-            other.gameObject.transform.position = new Vector3(box_spawn.transform.position.x, box_spawn.transform.position.y, 9);
-            other.gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
-            other.gameObject.GetComponent<Rigidbody2D>().gravityScale = 1f;
-            box_break_SFX.Play();
+            StartCoroutine(ResetBox(other.gameObject));
+
+            
         }
+    }
+
+    IEnumerator ResetBox(GameObject box){
+        box_break_effect_obj.transform.position = box.transform.position;
+        box_break_effect_obj.GetComponent<ParticleSystem>().Play();
+
+        yield return new WaitForSeconds(0.01f);
+
+        box.transform.position = new Vector3(box_spawn.transform.position.x, box_spawn.transform.position.y, 9);
+        box.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        box.GetComponent<Rigidbody2D>().gravityScale = 1f;
+        box_break_SFX.Play();
     }
 }
